4f9851199de420464fa9e84902b8c6affd13895a
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-02-22 12:27:19 +0100

    Update MobileCollage.tsx

--- src/components/MobileCollage.tsx
+++ src/components/MobileCollage.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "gatsby";
 
 function MobileCollage({ className, images }: any) {
     const slicedImages = () => {
         return images.slice(0, 3);
     };
     return (
-        <div className={className + " " + "md:hidden"}>
-            {slicedImages().map((img: any) => {
-                return <img src={img} className=" h-[180px] rounded-lg"></img>;
+        <div className={className + " " + "gap*4 flex md:hidden"}>
+            {slicedImages().map((obj: any) => {
+                return (
+                    <Link to={obj.url} className="w-full">
+                        <img src={obj.img} className="w-full rounded-lg"></img>
+                    </Link>
+                );
             })}
         </div>
     );
