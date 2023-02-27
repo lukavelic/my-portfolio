c49619c71947b8be2fb15f9c23a27ad17eda403c
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-02-27 11:55:43 +0100

    Update PopAnimation.tsx

--- src/components/PopAnimation.tsx
+++ src/components/PopAnimation.tsx
@@ -4,13 +4,12 @@ import { useInView } from "react-intersection-observer";
 const PopAnimation = ({ children }: any) => {
     const { ref, inView, entry } = useInView({
         triggerOnce: true,
-        threshold: 0,
+        threshold: 0.25,
     });
 
     return (
-        <div className=" overflow-hidden">
+        <div className=" overflow-hidden" ref={ref}>
             <div
-                ref={ref}
                 className={
                     inView
                         ? "translate-y-0 opacity-100 transition-all duration-1000"
